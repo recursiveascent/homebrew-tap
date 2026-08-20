de1eb46f337b62f81d680f5c0b2a7a7601550826
Paul Smith <paulsmith@gmail.com>
2026-08-20 14:47:43 +0000

    litefind v0.1.1

--- Formula/litefind.rb
+++ Formula/litefind.rb
@@ -1,8 +1,8 @@
 class Litefind < Formula
   desc "Search your SQLite databases"
   homepage "https://github.com/recursiveascent/litefind"
-  url "https://github.com/recursiveascent/litefind/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2dc509bc7d27e81514ca781e1aa916629d248d18f271f63cbae90bdc73ca3659"
+  url "https://github.com/recursiveascent/litefind/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "c6470a2cf512adbb02029969b0d5baa251dcf5550587e5a61092fcaf4362ac33"
   license "MIT"
 
   depends_on "go" => :build
